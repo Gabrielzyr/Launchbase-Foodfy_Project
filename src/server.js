a24e8e10a1f2f4de2f9a259df3c057d85cfc2442
GabrielZyr <gabrielmz397@gmail.com>
2020-07-23 00:51:34 -0300

    preparei a pág. que leva a receita

--- src/server.js
+++ src/server.js
@@ -26,4 +26,17 @@ server.get('/receitas', function(req, res) {
     return res.render('recipes.njk', {recipes: recipes})
 })
 
+server.get("/receita", function(req, res) {
+    return res.render('receita.njk')
+})
+
+server.get("/receitas/:index", function (req, res) {
+    const recipes = []; // Array de receitas carregadas do data.js
+    const recipeIndex = req.params.index;
+  
+    console.log(recipes[recipeIndex]);
+  })
+
+  //render temporário
+
 server.listen(3000)
