95eb3fe3b0e61de810d1351cd09ff2ef36afb84b
GabrielZyr <gabrielmz397@gmail.com>
2020-07-29 20:32:54 -0300

    adm recipes list and details

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,40 @@
+// const recipesData = require('../src/database/data')
+const recipesData = require('../src/database/data.json')
 
 
 exports.index = function(req, res) {
-    return res.render('admin/index')
+    return res.render('admin/index', {recipes: recipesData.recipes})
 }
+
+// exports.show = function(req, res) {
+//     return res.render("admin/details")
+// }
+
+exports.show = function (req, res) {
+    const { id } = req.params
+    const getRecipe = recipesData.recipes.find(function(recipe) {
+        return recipe.id == id
+    })
+    if (!getRecipe) return res.send('Recipe not found!')
+    const recipe = {
+        ...getRecipe
+    }
+    
+    return res.render('admin/details', { recipe })
+}
+
+// exports.show = function(req,res) {
+//     const { id } = req.params
+//     const foundInstructor = data.instructors.find(function(instructor){
+//         return instructor.id == id
+//     })
+//     if (!foundInstructor) return res.send('instructor not found')
+
+//     const instructor = {
+//         ...foundInstructor,
+//         age: age(foundInstructor.birth),
+//         services: foundInstructor.services.split(","),
+//         created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
+//     }
+//     return res.render('instructors/show', { instructor })
+// }
--- routes.js
+++ routes.js
@@ -24,7 +24,7 @@ routes.get("/admin", function(req, res) {
 
 routes.get("/admin/recipes", admin.index); // Mostrar a lista de receitas
 // routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
-// routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id", admin.show); // Exibir detalhes de uma receita
 // routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
 
 // routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
