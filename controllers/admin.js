f668450afa58ae753a80fffd0e35d2c93a486bdb
GabrielZyr <gabrielmz397@gmail.com>
2020-07-30 19:50:00 -0300

    add create and edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,28 +1,95 @@
-// const recipesData = require('../src/database/data')
+const fs = require("fs")
 const recipesData = require('../src/database/data.json')
 
 
-exports.index = function(req, res) {
-    return res.render('admin/index', {recipes: recipesData.recipes})
+exports.index = function (req, res) {
+    return res.render('admin/index', { recipes: recipesData.recipes })
 }
 
-// exports.show = function(req, res) {
-//     return res.render("admin/details")
-// }
+exports.create = function (req, res) {
+    return res.render("admin/create")
+}
+
+exports.post = function (req, res) {
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please, fill all fiels')
+        }
+    }
+    let { image, title, author, ingredients, methods, information } = req.body
+
+    id = Number(recipesData.recipes.length + 1)
+    // let id = 1
+    // const lastRecipe = recipesData.recipes[recipesData.recipes.length - 1]
+
+    // if (lastRecipe){
+    //     id = lastRecipe.id + 1
+    // }
+
+    recipesData.recipes.push({
+        id,
+        image,
+        title,
+        author,
+        ingredients,
+        methods,
+        information
+    })
+    fs.writeFile("src/database/data.json", JSON.stringify(recipesData, null, 2), function (err) {
+        if (err) return res.send('write file error!')
+        return res.redirect(`/admin/recipes/${id}`)
+    })
+
+}
 
 exports.show = function (req, res) {
     const { id } = req.params
-    const getRecipe = recipesData.recipes.find(function(recipe) {
+    const getRecipe = recipesData.recipes.find(function (recipe) {
         return recipe.id == id
     })
     if (!getRecipe) return res.send('Recipe not found!')
     const recipe = {
         ...getRecipe
     }
-    
+
     return res.render('admin/details', { recipe })
 }
 
 exports.edit = function (req, res) {
-    return res.render("admin/edit")
+    const { id } = req.params
+
+    const foundRecipe = recipesData.recipes.find(function(recipe){
+        return recipe.id == id
+    })
+    if (!foundRecipe) return res.send('instructor not found')
+    
+    const recipe = {
+        ...foundRecipe
+    }
+    
+    return res.render("admin/edit", { recipe })
+}
+
+exports.put = function (req, res) {
+    const { id } = req.body
+    let index = 0
+    const foundRecipe = recipesData.recipes.find(function(recipe, foundIndex){
+        if (id == recipe.id) {
+            index = foundIndex
+            return true
+        }
+    })
+    if (!foundRecipe) return res.send('instructor not found')
+    
+    const recipe = {
+        id: Number(req.body.id),
+        ...foundRecipe
+    }
+    recipesData.recipes[index] = recipe
+    fs.writeFile("src/database/data.json", JSON.stringify(recipesData, null, 2), function(err) {
+        if(err) return res.send("write file error!")
+        return res.redirect(`/admin/recipes/${id}`)
+    })
 }
--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
-const recipesData = require('../src/database/data')
+const recipesData = require('../src/database/data.json')
 
 
 
 exports.index = function(req, res) {
-    return res.render('recipes/index', {recipes: recipesData})
+    return res.render('recipes/index', {recipes: recipesData.recipes})
 }
 
 exports.about = function(req, res) {
@@ -12,12 +12,12 @@ exports.about = function(req, res) {
 }
 
 exports.list = function(req, res) {
-    return res.render('recipes/recipes', {recipes: recipesData})
+    return res.render('recipes/recipes', {recipes: recipesData.recipes})
 }
 
 exports.recipe = function (req, res) {
     const recipe = []; // Array de receitas carregadas do data.js
     const recipeIndex = req.params.index;
-    return res.render('recipes/recipe-page', { recipe: recipesData[recipeIndex]})
+    return res.render('recipes/recipe-page', { recipe: recipesData.recipes[recipeIndex]})
     // console.log(recipes[recipeIndex]);
 }
